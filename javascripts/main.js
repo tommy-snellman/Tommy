9c47e2df96ac5cf17df14cee262b13f378b59a40
tommy-snellman <tommy.sn95@gmail.com>
2013-12-02 14:11:19 +0200

    advent calendar

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,3 +1,8 @@
 jQuery(document).ready(function() {
-	
+	var today = new Date();
+	var d = today.getDate();
+
+	for(var i = d + 1; i < 32; i++) {
+		$(".window-" + i).css("display", "none");
+	}
 });
