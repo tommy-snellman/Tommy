83d8fba77c2855c9623265bc1fdb3e14f32c19e4
tommy-snellman <tommy.sn95@gmail.com>
2013-12-03 10:51:47 +0200

    Added descriptive comments to js

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function() {
 	var today = new Date(); // Date() = output the date - "Tue Dec 03 2013 10:44:32 GMT+0200 (FLE, normaltid)"
 	var d = today.getDate(); // getDate() = outputs only the day's number - 3
 
-// Compares today's date and output only the once that has been - If today is Dec 3, it'll output Dec 1 -> Dec 3
+// Compares today's date and output only the ones that has been and current day - If today is Dec 3, it'll output Dec 1 -> Dec 3
 	for(var i = d + 1; i < 25; i++) {
 		$(".window-" + i).css("display", "none");
 	}
