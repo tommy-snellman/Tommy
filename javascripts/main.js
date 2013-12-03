18156c0c4c58ff1f16d3af405a2e98aa86aea18f
tommy-snellman <tommy.sn95@gmail.com>
2013-12-03 10:50:09 +0200

    Added descriptive comments to js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,13 @@
+//  Script for Advent Calendar!
 jQuery(document).ready(function() {
-	var today = new Date();
-	var d = today.getDate();
+	var today = new Date(); // Date() = output the date - "Tue Dec 03 2013 10:44:32 GMT+0200 (FLE, normaltid)"
+	var d = today.getDate(); // getDate() = outputs only the day's number - 3
 
+// Compares today's date and output only the once that has been - If today is Dec 3, it'll output Dec 1 -> Dec 3
 	for(var i = d + 1; i < 25; i++) {
 		$(".window-" + i).css("display", "none");
 	}
+	// Puts bold on the current day and remove underline from all others
 	for(var x = 1; x < 25; x++) {
 		if(x != d) {
 			$(".window-" + x + ">").css("text-decoration", "none");
@@ -12,7 +15,7 @@ jQuery(document).ready(function() {
 			$(".window-" + x + ">").css("font-weight", "bold");
 		}
 	}
-
+	// Current day blinks 15 times
 	for(var i = 0; i < 15; i++) {
     	$(".window-" + d + ">").fadeTo('slow', 0.2).fadeTo('slow', 3.0);
   	}
